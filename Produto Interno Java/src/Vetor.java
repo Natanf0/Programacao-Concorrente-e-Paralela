87336893b2cac95c1c1d20332981fea5da8e1d07
Natan Ferreira <natan16ferreira@gmail.com>
2025-04-06 21:47:18 -0300

    versão paralela em java

---
+++ Produto Interno Java/src/Vetor.java
@@ -0,0 +1,12 @@
+public class Vetor {
+    private final int linhas;
+    private final char nome;
+
+    public Vetor(int linhas, char nome) {
+        this.linhas = linhas;
+        this.nome = nome;
+    }
+
+    public int getLinhas() {return linhas;}
+    public char getNome() {return nome;}
+}
